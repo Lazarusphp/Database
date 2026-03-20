45e55c2fce6e1bf6b07a6a49c9ee726d71d4fbac
mbamber1986 <30671636+mbamber1986@users.noreply.github.com>
2026-03-20 06:52:39 +0000

    Updated Connection.php

--- src/Connection.php
+++ src/Connection.php
@@ -1,16 +1,19 @@
 <?php
 namespace LazarusPhp\Database;
 
+use PDO;
 use PDOStatement;
+use RuntimeException;
 
 abstract class Connection
 {
     private static array $config = [];
     private PDOStatement $connection;
+    private static ?PDO $pdo = null;
     
     public static function make(?string $type=null,?string $hostname=null,?string $username=null,?string $password=null,?string $dbname=null):void
     {
-          self::$config = 
+        self::$config = 
         [
             "type"=>($type ?? $_ENV["type"]),
             "hostname"=>($hostname ?? $_ENV["hostname"]),
@@ -18,6 +21,40 @@ abstract class Connection
             "password"=>($password ?? $_ENV["password"]),
             "dbname"=>($dbname ?? $_ENV["dbname"]),
         ];
+    }
+
+
+
+    public static function get()
+    {
+        if(!self::$pdo)
+        {
+            if(empty(self::$config))
+            {
+                throw new RuntimeException("Connection Not Made : Call `Connection::make()` first ");
+            }
+
+            self::$pdo = self::connect();
+        }
+        
+        return self::$pdo;
+    }
+
+    private static function connect()
+    {
+        $config = self::$config;
+        $dsn = $config["type"].":host=".$config["hostname"].";dbname=".$config["dbname"];
+        return new PDO($dsn,$config["username"],$config["password"],self::options());
+    }
+
+    private static function options():array
+    {
+          return [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
     }
     
     public static function retrieve():array
